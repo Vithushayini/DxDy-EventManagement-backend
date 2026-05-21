906213643a6bda68d653df2792b72f24fc91d13a
Vithushayini <vithushayini2000@gmail.com>
2026-05-21 20:38:09 +0530

    db config added

---
+++ config/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export async function connectDatabase() {
+  const mongoUri = process.env.MONGODB_URI;
+
+  if (!mongoUri) {
+    throw new Error('MONGODB_URI is required');
+  }
+
+  mongoose.set('strictQuery', true);
+  await mongoose.connect(mongoUri);
+}
